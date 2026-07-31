33714b4ddf28319a69289a1858b9ee003c4a0db6
ChristianCFT <toledochristian272@gmail.com>
2026-07-30 22:52:46 -0300

    Modificando next.config.js para deploy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3001/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
       },
     ];
   },
